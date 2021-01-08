55244016374a3f455dc5f71f1fbd9698d7b2f43c
Jon Bramley <brammers@gmail.com>
2021-01-08 16:26:00 +0000

    feat: dataset of Bucks shapefiles

---
+++ collection/pipeline.mk
@@ -0,0 +1,61 @@
+DEVELOPMENT_POLICY_AREA_DATASET=$(DATASET_DIR)development-policy-area.csv
+DEVELOPMENT_POLICY_AREA_TRANSFORMED_FILES=\
+    $(TRANSFORMED_DIR)development-policy-area/07c85467a160b0ca3debf5a15f7c72007ee41503bdecd28d19aac050fff23ce0.csv\
+    $(TRANSFORMED_DIR)development-policy-area/1b482d0f57d12f6b3d1e77b040cb28bd47cde5768441422260b4aa6d8812be20.csv\
+    $(TRANSFORMED_DIR)development-policy-area/2a0fbb4617763bb2644fa489edcb36942cd07743dce4adfd4791e0ce37ca135a.csv\
+    $(TRANSFORMED_DIR)development-policy-area/33943492cbaa803a874b432e3d272a4b447611bdf56eac14d465f65ecff3892f.csv\
+    $(TRANSFORMED_DIR)development-policy-area/39c10fd7bd3348291170173c9a0c783a0108678613ccf51b1088d3ed0f932306.csv\
+    $(TRANSFORMED_DIR)development-policy-area/440c5c1392fda63f7c274768359741151560c2b696f1a38a3083cc7dca435958.csv\
+    $(TRANSFORMED_DIR)development-policy-area/6b2628128c516c2e07fc1f45d1794055985a4257dfbfca234bcd4846a2eddc0d.csv\
+    $(TRANSFORMED_DIR)development-policy-area/6d624bc688118d70d5ecbc41403c66b5bdb49d8ccd9f75411c4f7a6f287cf409.csv\
+    $(TRANSFORMED_DIR)development-policy-area/a9d2a9aa54e081dda1931efede4a616da4a0b1a7ad298ae162b023c892c11daf.csv\
+    $(TRANSFORMED_DIR)development-policy-area/b1d1b3a145b8f57b54a6227dc8b85cacce48cb9468efb61dbaa978facc48560d.csv\
+    $(TRANSFORMED_DIR)development-policy-area/bdeb9d9f3e085c5e97dceaeff291a48ea74d21185dce3c8aca5ae14b3b93dc47.csv\
+    $(TRANSFORMED_DIR)development-policy-area/bf15ed030a1201b4ba9359ad051fe920965102701cad70e7c1af2d4cf0ae81f3.csv\
+    $(TRANSFORMED_DIR)development-policy-area/e936b1eca6f85c2e40b830e16b7f799901d0fa87a6631d529c0946696138931f.csv
+
+$(TRANSFORMED_DIR)development-policy-area/07c85467a160b0ca3debf5a15f7c72007ee41503bdecd28d19aac050fff23ce0.csv: collection/resource/07c85467a160b0ca3debf5a15f7c72007ee41503bdecd28d19aac050fff23ce0
+	$(run-pipeline)
+
+$(TRANSFORMED_DIR)development-policy-area/1b482d0f57d12f6b3d1e77b040cb28bd47cde5768441422260b4aa6d8812be20.csv: collection/resource/1b482d0f57d12f6b3d1e77b040cb28bd47cde5768441422260b4aa6d8812be20
+	$(run-pipeline)
+
+$(TRANSFORMED_DIR)development-policy-area/2a0fbb4617763bb2644fa489edcb36942cd07743dce4adfd4791e0ce37ca135a.csv: collection/resource/2a0fbb4617763bb2644fa489edcb36942cd07743dce4adfd4791e0ce37ca135a
+	$(run-pipeline)
+
+$(TRANSFORMED_DIR)development-policy-area/33943492cbaa803a874b432e3d272a4b447611bdf56eac14d465f65ecff3892f.csv: collection/resource/33943492cbaa803a874b432e3d272a4b447611bdf56eac14d465f65ecff3892f
+	$(run-pipeline)
+
+$(TRANSFORMED_DIR)development-policy-area/39c10fd7bd3348291170173c9a0c783a0108678613ccf51b1088d3ed0f932306.csv: collection/resource/39c10fd7bd3348291170173c9a0c783a0108678613ccf51b1088d3ed0f932306
+	$(run-pipeline)
+
+$(TRANSFORMED_DIR)development-policy-area/440c5c1392fda63f7c274768359741151560c2b696f1a38a3083cc7dca435958.csv: collection/resource/440c5c1392fda63f7c274768359741151560c2b696f1a38a3083cc7dca435958
+	$(run-pipeline)
+
+$(TRANSFORMED_DIR)development-policy-area/6b2628128c516c2e07fc1f45d1794055985a4257dfbfca234bcd4846a2eddc0d.csv: collection/resource/6b2628128c516c2e07fc1f45d1794055985a4257dfbfca234bcd4846a2eddc0d
+	$(run-pipeline)
+
+$(TRANSFORMED_DIR)development-policy-area/6d624bc688118d70d5ecbc41403c66b5bdb49d8ccd9f75411c4f7a6f287cf409.csv: collection/resource/6d624bc688118d70d5ecbc41403c66b5bdb49d8ccd9f75411c4f7a6f287cf409
+	$(run-pipeline)
+
+$(TRANSFORMED_DIR)development-policy-area/a9d2a9aa54e081dda1931efede4a616da4a0b1a7ad298ae162b023c892c11daf.csv: collection/resource/a9d2a9aa54e081dda1931efede4a616da4a0b1a7ad298ae162b023c892c11daf
+	$(run-pipeline)
+
+$(TRANSFORMED_DIR)development-policy-area/b1d1b3a145b8f57b54a6227dc8b85cacce48cb9468efb61dbaa978facc48560d.csv: collection/resource/b1d1b3a145b8f57b54a6227dc8b85cacce48cb9468efb61dbaa978facc48560d
+	$(run-pipeline)
+
+$(TRANSFORMED_DIR)development-policy-area/bdeb9d9f3e085c5e97dceaeff291a48ea74d21185dce3c8aca5ae14b3b93dc47.csv: collection/resource/bdeb9d9f3e085c5e97dceaeff291a48ea74d21185dce3c8aca5ae14b3b93dc47
+	$(run-pipeline)
+
+$(TRANSFORMED_DIR)development-policy-area/bf15ed030a1201b4ba9359ad051fe920965102701cad70e7c1af2d4cf0ae81f3.csv: collection/resource/bf15ed030a1201b4ba9359ad051fe920965102701cad70e7c1af2d4cf0ae81f3
+	$(run-pipeline)
+
+$(TRANSFORMED_DIR)development-policy-area/e936b1eca6f85c2e40b830e16b7f799901d0fa87a6631d529c0946696138931f.csv: collection/resource/e936b1eca6f85c2e40b830e16b7f799901d0fa87a6631d529c0946696138931f
+	$(run-pipeline)
+
+$(DEVELOPMENT_POLICY_AREA_DATASET): $(DEVELOPMENT_POLICY_AREA_TRANSFORMED_FILES)
+	$(build-dataset)
+
+transformed:: $(DEVELOPMENT_POLICY_AREA_TRANSFORMED_FILES)
+
+dataset:: $(DEVELOPMENT_POLICY_AREA_DATASET)
